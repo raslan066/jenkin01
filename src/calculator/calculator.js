f8f595525f231bc7e0acce64e577177137f0bc82
raslan <AusV22020>
2020-04-20 10:48:41 -0500

    inital commit

---
+++ src/calculator/calculator.js
@@ -0,0 +1,11 @@
+function calcularor() {
+    function sum(a,b) {
+
+    return a +b;
+
+    }
+
+    return {sum}
+
+}
+module.exports = calcularor;
---
+++ test/unit/calculator.js
@@ -0,0 +1,18 @@
+const app = require('../../src/calculator/calculator.js')
+const assert = require('assert')
+
+describe('Test Suite For Calculator',() =>{
+
+    describe('Addiditon Testing',()=>{
+
+        it('1+2 = 3', ()=> {
+            assert.equal(app().sum(1,2),3)
+        })
+        it('2+2 = 4', ()=> {
+            assert.equal(app().sum(2,2),4)
+        })
+        it('3+2 = 5', ()=> {
+            assert.equal(app().sum(3,2),5)
+        })
+    })
+})
